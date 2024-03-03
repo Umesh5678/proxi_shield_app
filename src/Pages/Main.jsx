2fe18ef1407cd62f180d3658586c1b3f33003cba
Divya <amatedivya2@gmail.com>
2024-03-03 16:57:38 +0530

    'first_commit'

--- src/Pages/Main.jsx
+++ src/Pages/Main.jsx
@@ -16,6 +16,9 @@ import Repair from "./Repair.jsx";
 import Admixture from "./Admixture.jsx";
 import Grouts from "./Grouts.jsx";
 import Building from "./Building.jsx";
+import Nav from "./Nav.jsx";
+import Technicaldatasheets from "./Technicaldatasheets.jsx";
+import Saleresponsive from "./Saleresponsive.jsx";
 
 
 
@@ -23,16 +26,28 @@ export default function Main(){
     return(
         <>
         <Router>
-            <Navbar></Navbar>
+            {/* <Navbar></Navbar> */}
+            <Nav></Nav>
             <Routes>
                 <Route path="/" element={<Index></Index>}></Route>
                 <Route path="/Company" element={<Company></Company>}></Route>
                 <Route path="/Product" element={<Product></Product>}></Route>
-                {/* <Route path="/Saleresponsive" element={<Saleresponsive></Saleresponsive>}></Route> */}
                 <Route path="/Contact" element={<Contact></Contact>}></Route>
+                <Route path="/Waterproofing" element={<Waterproofing></Waterproofing>}></Route>
+                <Route path="/Flooring" element={<Flooring></Flooring>}></Route>
+                <Route path="/Repair" element={<Repair></Repair>}></Route>
+                <Route path="/Building" element={<Building></Building>}></Route>
+                <Route path="/Admixture" element={<Admixture></Admixture>}></Route>
+                <Route path="/Grouts" element={<Grouts></Grouts>}></Route>
+                <Route path="/Technicaldatasheets" element={<Technicaldatasheets></Technicaldatasheets>}></Route>
+                <Route path="/Saleresponsive" element={<Saleresponsive></Saleresponsive>}></Route>
+                <Route path="/Event" element={<Event></Event>}></Route>
+                <Route path="/Videolib" element={<Videolib></Videolib>}></Route>
             </Routes>
             <Footer></Footer>       
         </Router>
+        {/* <Nav></Nav> */}
+        {/* <Navbar></Navbar> */}
         {/* <Event></Event> */}
         {/* <Videolib></Videolib> */}
         {/* <Waterproofing></Waterproofing> */}
